15e0f56d5f832929316f0c345331c55f629e9a36
Trevor Hansen <trevor.hansen@itas.ca>
2024-02-07 12:36:38 -0800

    Trevor.Hansen.py

---
+++ Trevor.Hansen.py
@@ -0,0 +1,11 @@
+
+
+def name_show():
+    
+    print("Trevor Hansen, 2024-02-07, Itas 164")
+    
+name_show()
+    
+if __name__=="__main__":
+ Message = input("Hello, Welcome back")
+ print(name_show(Message))
